ad001d96bcbaa3aed3aeffd8ddfcf5024b836623
imcpeek <iansmcpeek@gmail.com>
2018-04-15 18:41:51 -0700

    added post method for courses

--- src/main/java/com/vishooter/collegehelp/demo/controllers/CourseController.java
+++ src/main/java/com/vishooter/collegehelp/demo/controllers/CourseController.java
@@ -3,10 +3,9 @@ package com.vishooter.collegehelp.demo.controllers;
 import com.vishooter.collegehelp.demo.models.Course;
 import com.vishooter.collegehelp.demo.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path="/courses")
@@ -17,5 +16,11 @@ public class CourseController {
     @GetMapping(path = "")
     public @ResponseBody Iterable<Course> getCourses() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    public @ResponseBody ResponseEntity<Course> addCourse(@RequestBody Course course) {
+        Course savedCourse = courseRepository.save(course);
+        return ResponseEntity.ok(savedCourse);
     }
 }
--- src/main/java/com/vishooter/collegehelp/demo/models/Course.java
+++ src/main/java/com/vishooter/collegehelp/demo/models/Course.java
@@ -3,12 +3,14 @@ package com.vishooter.collegehelp.demo.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Entity
 @Table(name="courses")
 public class Course {
@@ -17,5 +19,5 @@ public class Course {
     private Integer id;
     private String name;
     private Integer credits;
-
+    private Integer school_id;
 }
